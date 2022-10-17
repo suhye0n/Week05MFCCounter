5bd67ce7940451c0dacc9c7b3136d20b1696e4a7
suhye0n <contact.suhyeon@gmail.com>
2022-10-17 13:31:51 +0900

    초기 커밋.

---
+++ Week05MFCCounter/Week05MFCCounter.h
@@ -0,0 +1,32 @@
+﻿
+// Week05MFCCounter.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CWeek05MFCCounterApp:
+// 이 클래스의 구현에 대해서는 Week05MFCCounter.cpp을(를) 참조하세요.
+//
+
+class CWeek05MFCCounterApp : public CWinApp
+{
+public:
+	CWeek05MFCCounterApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CWeek05MFCCounterApp theApp;
---
+++ Week05MFCCounter/Week05MFCCounterDlg.h
@@ -0,0 +1,34 @@
+﻿
+// Week05MFCCounterDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CWeek05MFCCounterDlg 대화 상자
+class CWeek05MFCCounterDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CWeek05MFCCounterDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WEEK05MFCCOUNTER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
